b1f9a7fbdf4c0b5236669d28b1ec7021e7871c8f
Arkaitus <braian.codex@gmail.com>
2021-08-27 09:18:39 -0300

    Adicionando pasta Introducao-JavaScript

---
+++ Fundamentos/Bloco04-JavaScript-Primeiros-passos/Introducao-JavaScript/base.js
@@ -0,0 +1,10 @@
+const base = 5;
+var height = 8;
+var area = 0;
+
+area = (base * height);
+
+perimeter = base + base + height + height;
+
+console.log(area);
+console.log(perimeter);
---
+++ Fundamentos/Bloco04-JavaScript-Primeiros-passos/Introducao-JavaScript/script.js
@@ -0,0 +1,7 @@
+const myName = "Braian";
+const birthCity = "Caxias";
+var birthYear = 1989;
+birthYear = 2030;
+
+
+console.log(myName, birthCity, birthYear);
